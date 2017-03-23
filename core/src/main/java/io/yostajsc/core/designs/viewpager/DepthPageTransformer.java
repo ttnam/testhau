5ff5138a6e63be4c62a0366b1571060f45961e7f
Phuc-Hau Nguyen <nphau.itus@gmail.com>
2017-03-23 13:30:47 +0700

    [f] add transform

--- core/src/main/java/io/yostajsc/core/designs/viewpager/DepthPageTransformer.java
+++ core/src/main/java/io/yostajsc/core/designs/viewpager/DepthPageTransformer.java
@@ -1,41 +1,31 @@
 package io.yostajsc.core.designs.viewpager;
 
-import android.support.v4.view.ViewPager;
 import android.view.View;
 
-public class DepthPageTransformer implements ViewPager.PageTransformer {
+public class DepthPageTransformer extends ViewPagerBaseTransformer {
+
     private static final float MIN_SCALE = 0.75f;
 
-    public void transformPage(View view, float position) {
-        int pageWidth = view.getWidth();
-
-        if (position < -1) { // [-Infinity,-1)
-            // This page is way off-screen to the left.
-            view.setAlpha(0);
-
-        } else if (position <= 0) { // [-1,0]
-            // Use the default slide transition when moving to the left page
-            view.setAlpha(1);
-            view.setTranslationX(0);
-            view.setScaleX(1);
-            view.setScaleY(1);
-
-        } else if (position <= 1) { // (0,1]
-            // Fade the page out.
+    @Override
+    protected void onTransform(View view, float position) {
+        if (position <= 0f) {
+            view.setTranslationX(0f);
+            view.setScaleX(1f);
+            view.setScaleY(1f);
+        } else if (position <= 1f) {
+            final float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
             view.setAlpha(1 - position);
-
-            // Counteract the default slide transition
-            view.setTranslationX(pageWidth * -position);
-
-            // Scale the page down (between MIN_SCALE and 1)
-            float scaleFactor = MIN_SCALE
-                    + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setPivotY(0.5f * view.getHeight());
+            view.setTranslationX(view.getWidth() * -position);
             view.setScaleX(scaleFactor);
             view.setScaleY(scaleFactor);
-
-        } else { // (1,+Infinity]
-            // This page is way off-screen to the right.
-            view.setAlpha(0);
         }
     }
+
+    @Override
+    protected boolean isPagingEnabled() {
+        return true;
+    }
+
+
 }
---
+++ core/src/main/java/io/yostajsc/core/designs/viewpager/StackTransformer.java
@@ -0,0 +1,16 @@
+package io.yostajsc.core.designs.viewpager;
+
+import android.view.View;
+
+/**
+ * Created by nphau on 3/23/17.
+ */
+
+public class StackTransformer extends ViewPagerBaseTransformer {
+
+    @Override
+    protected void onTransform(View view, float position) {
+        view.setTranslationX(position < 0 ? 0f : -view.getWidth() * position);
+    }
+
+}
