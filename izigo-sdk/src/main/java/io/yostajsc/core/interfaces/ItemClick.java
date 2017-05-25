0d7b92bd380c700dd49def61bbd6cbc073cb4e48
Phuc-Hau Nguyen <nphau.itus@gmail.com>
2017-05-25 14:01:05 +0700

    [r] remove unusual

--- izigo-sdk/src/main/java/io/yostajsc/core/interfaces/ItemClick.java
+++
@@ -1,9 +0,0 @@
-package io.yostajsc.core.interfaces;
-
-/**
- * Created by Phuc-Hau Nguyen on 2/19/2017.
- */
-
-public interface ItemClick<T, H> {
-    void onClick(T t, H h);
-}
--- izigo-sdk/src/main/java/io/yostajsc/core/interfaces/OnConnectionTimeoutListener.java
+++
@@ -1,9 +0,0 @@
-package io.yostajsc.core.interfaces;
-
-/**
- * Created by nphau on 3/19/17.
- */
-
-public interface OnConnectionTimeoutListener {
-    void onConnectionTimeout();
-}
